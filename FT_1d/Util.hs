59d9dc27ca21354d60908ec63a0253ccc0799d17
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-08-24 16:07:49 +0900

    fix code indentation

--- FT_1d/Util.hs
+++ FT_1d/Util.hs
@@ -19,19 +19,20 @@ onError filename _error = do
 
 --read from file
 programSourceFromFile :: IO String
-programSourceFromFile = do let clprogramName = "ft_1d.cl"
-                           E.catch (readFile clprogramName)
-                                 (onError clprogramName)
+programSourceFromFile = do
+  let clprogramName = "ft_1d.cl"
+  E.catch (readFile clprogramName)
+          (onError clprogramName)
 
 -- transform String to [CDouble]
 toNum :: String -> [CDouble]
 toNum = foldr toNumber [] . words
-      where toNumber xs number = read xs:number
+  where toNumber xs number = read xs:number
 
 -- transform [CDouble] to String
 toStrings :: [CDouble] -> String
 toStrings = unlines . foldr toStr []
-    where toStr xs number = show xs:number
+  where toStr xs number = show xs:number
 
 -- read input value from File
 numberFromFile :: IO String
@@ -40,8 +41,8 @@ numberFromFile = do
   if length args /= 0 then do
     let cltransformSource = head args
     E.catch (readFile cltransformSource)
-              (onError cltransformSource)
+            (onError cltransformSource)
   else do
     let cltransformSource = "input.txt"
     E.catch (readFile cltransformSource)
-              (onError cltransformSource)
+            (onError cltransformSource)
